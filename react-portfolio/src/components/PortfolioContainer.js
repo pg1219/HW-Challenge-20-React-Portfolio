ad828cd8d028c94cae41ca59a342022702b04140
pg1219 <peter.gronseth@gmail.com>
2023-07-18 23:06:43 -0400

    push

---
+++ react-portfolio/src/components/PortfolioContainer.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import NavTabs from './NavTabs';
+import About from './pages/About';
+import Portfolio from './pages/Portfolio';
+import Contact from '.pages/Contact';
+import Resume from './pages/Resume';
+
+export default function PortfolioContainer() {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  // TODO: Add a comment describing the functionality of this method
+  const renderPage = () => {
+    if (currentPage === 'Home') {
+      return <Home />;
+    }
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Blog') {
+      return <Blog />;
+    }
+    return <Contact />;
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
+  return (
+    <div>
+      {/* // TODO: Add a comment describing what we are passing as props */}
+      <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+      {/* // TODO: Add a comment explaining what is happening on the following line */}
+      {renderPage()}
+    </div>
+  );
+}
---
+++ react-portfolio/src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+       <h2 className="container">About Me</h2>
+          <p>My name is Peter and this is my portfolio. I am a bootcamp student learning to code for the first time. This portfolio will highlight my best efforts throughout the class.
+        </p>
+    </div>
+  );
+}
